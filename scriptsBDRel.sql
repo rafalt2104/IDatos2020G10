19a10c98d9540e47780a7d140ae4b17b847e21b2
rafalt2104 <rafalt2104@gmail.com>
2020-12-07 23:03:45 -0300

    Update scriptsBDRel.sql

--- scriptsBDRel.sql
+++ scriptsBDRel.sql
@@ -11,25 +11,25 @@ CREATE TABLE `emergencia` (
   `emergenciaURI` text,
   `emergenciaNombre` text,
   PRIMARY KEY (`emergenciaId`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `instituto` (
   `institutoId` int NOT NULL AUTO_INCREMENT,
   `instututoURI` text,
   `institutoNombre` text,
   PRIMARY KEY (`institutoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `localidad` (
   `localidadId` int NOT NULL AUTO_INCREMENT,
   `localidadURI` text NOT NULL,
   `localidadNombre` text,
   PRIMARY KEY (`localidadId`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `serviciosalud` (
   `serviciosaludId` int NOT NULL AUTO_INCREMENT,
   `servicioSaludURI` text,
   `servicioSaludNombre` text,
   PRIMARY KEY (`serviciosaludId`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
